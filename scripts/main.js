ba2df6bea77f170cd3fcde9391e261b039fbbc2f
Korshunova Anastasiia <korshunova.acp@bk.ru>
2025-04-28 22:49:59 +0300

    В кое-то веке доделала header

--- scripts/main.js
+++ scripts/main.js
@@ -6,17 +6,12 @@ document.addEventListener('DOMContentLoaded', function() {
     const burgerIcon = document.getElementById('burgerIcon');
     const mainNav = document.getElementById('mainNav');
     
-    // Обработчик клика по бургеру
     burgerIcon.addEventListener('click', function() {
-        // Переключаем класс active у меню
         mainNav.classList.toggle('active');
-        
-        // Анимация бургера в "крестик"
         burgerIcon.classList.toggle('active');
     });
     
-    // Закрываем меню при клике на ссылку
-    const navLinks = document.querySelectorAll('.main-nav a');
+    const navLinks = document.querySelectorAll('.main-nav a:not(.dropdown > a)');
     navLinks.forEach(link => {
         link.addEventListener('click', function() {
             mainNav.classList.remove('active');
@@ -27,11 +22,11 @@ document.addEventListener('DOMContentLoaded', function() {
 
 // ========== Выпадающее меню =======================================================================
 
-// Добавить обработчик для мобильного меню
 document.querySelectorAll('.dropdown > a').forEach(item => {
     item.addEventListener('click', (e) => {
         if (window.innerWidth <= 960) {
             e.preventDefault();
+            e.stopPropagation(); // Блокируем всплытие события
             item.parentElement.classList.toggle('active');
         }
     });
